aca08aa8cf842c0f2db442e90b08c20a0ca49d19
Cheysser <cheysser@gmail.com>
2024-10-07 19:28:51 +0800

    updated routes to use resources for posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
-
-  get 'posts', to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id', to: 'posts#show', as: 'post'
-  patch 'posts/:id', to: 'posts#update'
-  put 'posts/:id', to: 'posts#update'
-  delete 'posts/:id', to: 'posts#destroy'
+  resources :posts
+  # get 'posts', to: 'posts#index'
+  # post 'posts', to: 'posts#create'
+  # get 'posts/new', to: 'posts#new'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id', to: 'posts#show', as: 'post'
+  # patch 'posts/:id', to: 'posts#update'
+  # put 'posts/:id', to: 'posts#update'
+  # delete 'posts/:id', to: 'posts#destroy'
 end
