7d81fb9fad20e690ff0da9334fcd515afd5d4bab
Cheysser <cheysser@gmail.com>
2024-10-10 11:01:27 +0800

    added validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :title, length: { in: 4..100 }
+  validates :title, uniqueness: true
+  validates :content, presence: true, length: { minimum: 5, maximum: 4000 }
 end
