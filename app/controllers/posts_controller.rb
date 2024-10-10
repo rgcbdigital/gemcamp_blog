fc27a56f24802045739d41c82293a24ed04c25f0
Cheysser <cheysser@gmail.com>
2024-10-10 10:28:39 +0800

    updated post action colors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -8,37 +9,34 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    # render json: params[:post]
-    # @post = Post.new
-    # @post.title = params[:post][:title]
-    # @post.content = params[:post][:content]
-    # @post = Post.new(title: params[:post][:title],
-    #                  content: params[:post][:content])
-    @post = Post.new(params[:post].permit(:title, :content))
+    @post = Post.new(post_params)
+
     if @post.save
       redirect_to posts_path
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:title, :content))
+    if @post.update(post_params)
       redirect_to posts_path
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     if @post.destroy
       redirect_to posts_path
     end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
